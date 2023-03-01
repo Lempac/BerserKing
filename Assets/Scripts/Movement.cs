f914c6f4dd17b19597abaa4ed5c5671f5a6e899a
22DP3BBiks <13DPBBiksis@rvt.lv>
2023-03-01 18:37:56 +0200

    movement and something

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float Speed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, Input.GetAxis("Vertical") * Speed * Time.deltaTime));
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, Input.GetAxis("Vertical") * Speed * Time.deltaTime),ForceMode2D.Impulse);
     }
 }
