ae6f6dbc81d881451c034c02d20eee49ab900be5
22DP3APaza <13DPAPazars@rvt.lv>
2023-03-02 16:38:30 +0200

    added speed sprite

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -9,5 +10,16 @@ public class Movement : MonoBehaviour
     void FixedUpdate()
     {
         GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, Input.GetAxis("Vertical") * Speed * Time.deltaTime),ForceMode2D.Impulse);
+    }
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if(collision.name == "speed")
+        {
+            Speed += 10f;
+            Destroy(collision.gameObject);
+        }
+
     }
 }
