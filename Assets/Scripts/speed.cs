fe590f215934066fe695d12fd26482a29e965a16
22DP3BBiks <13DPBBiksis@rvt.lv>
2023-03-03 20:45:13 +0200

    Something with ui

--- Assets/Scripts/speed.cs
+++ Assets/Scripts/speed.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class speed : MonoBehaviour
 {
     private void OnDestroy()
     {
+        if (!gameObject.scene.isLoaded) return;
         transform.position = new Vector3(0,2);
         GameObject clonename = Instantiate(gameObject);
         clonename.SetActive(true);
