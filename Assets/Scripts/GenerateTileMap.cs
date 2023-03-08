99540145da5c73f08e84131e0a6505c2b6196d33
Lempac <13DPBBiksis@rvt.lv>
2023-03-08 22:43:40 +0200

    Beginning for some loading

--- Assets/Scripts/GenerateTileMap.cs
+++ Assets/Scripts/GenerateTileMap.cs
@@ -2,20 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
 
 public class GenerateTileMap : MonoBehaviour
 {
+    [SerializeField] private Transform player;
     public TileBase[] baseTiles;
+    private Dictionary<Tuple<int,int>, byte> data = new Dictionary<Tuple<int, int>, byte>();
     GameObject Level;
     Tilemap LevelMap;
     private int ChunkX = 16, ChunkY = 16;
     [SerializeField] float delta = 0.01f;
-    async void Chunk(int PositionX, int PositionY)
+    void LoadChunk(int PositionX, int PositionY)
     {
-        //Tile[] list = new Tile[ChunkX*ChunkY];
         int StartX = PositionX * ChunkX;
         int StartY = PositionY * ChunkY;
         for (int x = StartX; x < StartX + ChunkX; x++)
@@ -24,33 +27,38 @@ public class GenerateTileMap : MonoBehaviour
             {
                 float i = Mathf.PerlinNoise(x * delta, y * delta);
                 LevelMap.SetTile(new Vector3Int(x, y), (i < .3f ? baseTiles[0] : (i >= .3f && i <= .7f  ? baseTiles[1] : baseTiles[2])));
-                await Task.Yield();
             }
         }
         Debug.Log($"Chunk {PositionX} : {PositionY} loaded!");
+    }
+    void UnloadChunk(int PositionX, int PositionY)
+    {
+        int StartX = PositionX * ChunkX;
+        int StartY = PositionY * ChunkY;
         for (int x = StartX; x < StartX + ChunkX; x++)
         {
             for (int y = StartY; y < StartY + ChunkY; y++)
             {
-                TileBase curr = LevelMap.GetTile(new Vector3Int(x, y));
-                //Debug.Log(curr.GetTileData);
-                //if(curr.name)
-                await Task.Yield();
+                LevelMap.SetTile(new Vector3Int(x, y), null);
             }
         }
     }
-    
+
+
     void Start()
     {
         Level = new GameObject("GenLevel", typeof(Tilemap), typeof(TilemapRenderer));
         Level.transform.parent = transform;
         LevelMap = Level.GetComponent<Tilemap>();
-        for (int x = 0; x < 10; x++)
+    }
+    private void FixedUpdate()
+    {
+        int x = Mathf.FloorToInt(player.position.x / ChunkX), y = Mathf.FloorToInt(player.position.y / ChunkY);
+        if (!data.TryGetValue(new Tuple<int, int>(x, y), out byte _))
         {
-            for (int y = 0; y < 10; y++)
-            {
-                Chunk(x, y);
-            }
+            LoadChunk(x, y);
+            data.Add(new Tuple<int, int>(x, y), 0b0);
         }
+
     }
 }
