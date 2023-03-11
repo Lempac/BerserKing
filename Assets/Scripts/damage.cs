d3dbe397bf33df4ea125c460470f861bb3ce00d4
22DP3APaza <13DPAPazars@rvt.lv>
2023-03-11 22:06:38 +0200

    added hp and timer

--- Assets/Scripts/damage.cs
+++ Assets/Scripts/damage.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class damage : MonoBehaviour
 {
+    public int damageperframe = 5;
+    public int hp = 100;
     private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (gameObject.name=="Player")
+        {
+            if (Time.frameCount % 6 == 0)
+            {
+                hp -= damageperframe;
+            }
+            
+            
+        }
     }
 }
