79e9961cbc59d8ff5db15fbe86ada520ef818265
22DP3APaza <13DPAPazars@rvt.lv>
2023-03-15 13:07:27 +0200

    added damage to particles and fixed some bugs with damage

--- Assets/Scripts/damage.cs
+++ Assets/Scripts/damage.cs
@@ -7,22 +7,25 @@ public class damage : MonoBehaviour
 {
     public int damageperframe = 5;
     public int hp = 100;
-    async void DoDamage()
+    async void DoDamage(Collision2D enemy)
     {
-        if (gameObject.name == "Player")
+
+        if (gameObject.name == "Player" && enemy.gameObject.name == "enemy")
+
         {
             hp -= damageperframe;
             await Task.Delay(500);
             cooldown = false;
         }
     }
+    
     bool cooldown = false;
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnCollisionStay2D(UnityEngine.Collision2D collision)
     {
         if (!cooldown)
         {
             cooldown = true;
-            DoDamage();
+            DoDamage(collision);
         }
     }
 }
