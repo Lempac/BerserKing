1db4ed879b24b77ab5d66ba4f8cc348c6b9d6648
22DP3APaza <13DPAPazars@rvt.lv>
2023-03-15 20:33:36 +0200

    commit fix

---
+++ Assets/Scripts/ChestMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ChestMenu : MonoBehaviour
+{
+    public TMP_Text ChestTitle;
+    public Button Take;
+}
