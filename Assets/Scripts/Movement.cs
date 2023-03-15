2c9c006a58685d9fc7e6c08048b309a294e20570
Lempac <13DPBBiksis@rvt.lv>
2023-03-15 20:28:20 +0200

    Some more marge confiction

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,11 +6,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float Speed = 1f;
-<<<<<<< HEAD
-=======
     public Animator animator;
-    // Update is called once per frame
->>>>>>> 2063117ee1183c20e8e609e156a4763ca2837311
     void FixedUpdate()
     {
         GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, Input.GetAxis("Vertical") * Speed * Time.deltaTime),ForceMode2D.Impulse);
