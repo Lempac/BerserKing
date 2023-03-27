5225c1a09ab901b827155c603351fa2661f6485f
22DP3APaza <13DPAPazars@rvt.lv>
2023-03-27 22:33:21 +0300

    commiting second weapon (not finished! Just to show teacher the progress)

---
+++ Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace PathCreation.Examples
+{
+    // Moves along a path at constant speed.
+    // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
+    public class PathFollower : MonoBehaviour
+    {
+        public PathCreator pathCreator;
+        public EndOfPathInstruction endOfPathInstruction;
+        public float speed = 5;
+        float distanceTravelled;
+
+        void Start() {
+            if (pathCreator != null)
+            {
+                // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+                pathCreator.pathUpdated += OnPathChanged;
+            }
+        }
+
+        void Update()
+        {
+            if (pathCreator != null)
+            {
+                distanceTravelled += speed * Time.deltaTime;
+                Vector3 newPos = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                transform.position = newPos;
+
+                // Calculate the direction that the particle should face along the path
+                Vector3 direction = pathCreator.path.GetDirection(distanceTravelled);
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                
+            }
+        }
+
+        // If the path changes during the game, update the distance travelled so that the follower's position on the new path
+        // is as close as possible to its position on the old path
+        void OnPathChanged() {
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Chainsaw_Particle.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chainsaw_Particle : MonoBehaviour
+{
+    public float damage = 10f;
+    public ParticleSystem particleSystem;
+    int events;
+
+    List<ParticleCollisionEvent> colEvents = new List<ParticleCollisionEvent>();
+
+    private void OnParticleCollision(GameObject other)
+    {
+        events = particleSystem.GetCollisionEvents(other, colEvents);
+
+
+        if (other.TryGetComponent(out Enemy en))
+        {
+
+            en.TakeDamage(damage);
+        }
+    }
+}
